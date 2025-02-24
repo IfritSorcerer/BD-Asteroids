e76ba925216a810b7dcd8aacc87c9fa6e5f242d7
Ian Isern <134439967+IfritSorcerer@users.noreply.github.com>
2025-02-24 13:56:34 -0500

    Step 7: Player Groups

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ def main():
    print(f"Screen height: {SCREEN_HEIGHT}")
    
    pygame.init()
+   updatable = pygame.sprite.Group()
+   drawable = pygame.sprite.Group()
+   
    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
    fpsClock = pygame.time.Clock()
    dt = 0
 
+   Player.containers = (updatable, drawable)
    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
    while True:
@@ -22,8 +26,12 @@ def main():
             return
 
       pygame.Surface.fill(screen, (0,0,0))
-      player.draw(screen)
-      player.update(dt)
+      
+      for p in updatable:
+         p.update(dt)
+
+      for p in drawable:
+         p.draw(screen)   
       pygame.display.flip()
 
       dt = fpsClock.tick(60) / 1000
