b2aa76ad1d12c2726a8a4418b038913881c0ab4e
Ian Isern <134439967+IfritSorcerer@users.noreply.github.com>
2025-02-24 13:19:23 -0500

    Step 4: Draw player to screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
    print(f"Starting asteroids!")
@@ -12,6 +13,8 @@ def main():
    fpsClock = pygame.time.Clock()
    dt = 0
 
+   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
    while True:
       for event in pygame.event.get():
          if event.type == pygame.QUIT:
@@ -19,6 +22,7 @@ def main():
             return
 
       pygame.Surface.fill(screen, (0,0,0))
+      player.draw(screen)
       pygame.display.flip()
 
       dt = fpsClock.tick(60) / 1000
